842d9fc1c8a44d77b0c181a6c610d0bd2ea9ff14
muqaddar <ppunter7@gmail.com>
2025-03-09 11:30:42 +0530

    results

--- nataij/settings.py
+++ nataij/settings.py
@@ -8,7 +8,7 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-2au_4jgf7*mxddvcblo(75=b(4ob7tw@i86m2*@!ifs_3k3owi'
-DEBUG = True  # Set to False in production, True for local testing
+DEBUG = False  # Set to False in production, True for local testing
 ALLOWED_HOSTS = ['*', '127.0.0.1', 'localhost']
 
 INSTALLED_APPS = [
